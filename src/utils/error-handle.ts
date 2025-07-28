77a0327066c600b6a4390237e408e743f9693f67
IbraAguero <ibrahimaguero@gmail.com>
2025-07-27 23:12:07 -0300

    feat: add error handler

---
+++ src/utils/error-handle.ts
@@ -0,0 +1,7 @@
+import { Response } from "express";
+
+export const handleHttp = (res: Response, error: string, errorRaw?: any) => {
+  res.status(500);
+  res.send({ error });
+  console.error(errorRaw);
+};
